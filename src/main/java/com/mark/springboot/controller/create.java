286239653c8178041880ff8a0cb3bc54382b8acd
chenqingqing <chenqingqing@iscas.ac.cn>
2022-01-19 23:43:11 +0800

    first commit

---
+++ src/main/java/com/mark/springboot/controller/create.java
@@ -0,0 +1,32 @@
+package com.mark.springboot.controller;
+
+
+import com.mark.springboot.http.R;
+import com.mark.springboot.service.CreateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * desc
+ *
+ * @author Lixf
+ * @since 2019/2/21 14:36
+ **/
+@RestController
+@RequestMapping("/test")
+public class create {
+    @Autowired
+    CreateService createService;
+
+    @Value("${dataName}")
+    private String dataName;//库名
+
+    @RequestMapping("/demo")
+    public R generate() {
+        createService.getTableInfo(dataName);
+        return R.ok();
+    }
+    // http://localhost:8888/test/demo
+}
